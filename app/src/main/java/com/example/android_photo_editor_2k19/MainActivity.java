6fa01fa78b114a261110f5d67239a34a580929fd
Jevgenijs Fedotenkovs <jevgenijs@example.com>
2019-03-25 16:27:38 +0200

    updated contrast

--- app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
+++ app/src/main/java/com/example/android_photo_editor_2k19/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.android_photo_editor_2k19;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private PictureThread thread;
     private Button bRight;
     private Button bLeft;
-
+    private static final int PICK_IMAGE_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,12 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageBitmap(bitmap);
 
         thread = new PictureThread(imageView,bitmap);
+
         thread.start();
+
+
+
+
 
         seekBarBrightness = (SeekBar) findViewById(R.id.seekBarBrightness);
         seekBarBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -115,4 +125,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void pick(View v) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case PICK_IMAGE_REQUEST:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+
+                    // method 1
+                    try {
+                        Bitmap fileBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        imageView.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    // method 2
+
+                    //try {
+                    //    InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                    //    Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+                    //    imageStream.close(;
+                    //   iv.setImageBitmap(yourSelectedImage);
+                    //} catch (FileNotFoundException e) {
+                    //    e.printStackTrace();
+                    //}
+
+                    // method 3
+                    // iv.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
 }
+
+
